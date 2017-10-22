de3347e5e707231bd137c2f4eef290ffd6d5cf71
shintiasari <32458115+shintiasari@users.noreply.github.com>
2017-10-21 22:34:24 -0700

    menghitung varaible yang di tentukan sebelumnnya

--- praktek 0204.cpp
+++ praktek 0204.cpp
@@ -8,4 +8,12 @@ int main(){
 	cout << " Luas Rumah			= "<<LuasRumah<<" m2"<<endl;
 	cout << " Luas 1 Box Parket		= "<<Luas1Box<<" m2"<<endl;
 	double JumlahBoxP, BiayaP, BiayaPG, TotalB;
+	JumlahBoxP = LuasRumah/Luas1Box;
+	cout <<" Jumlah Box Parket		= "<<JumlahBoxP<<endl;
+	BiayaP = JumlahBoxP*500;
+	cout<< " Biaya Parket (Rp) 		= "<<BiayaP<<endl;
+	BiayaPG = LuasRumah*20;
+	cout<<" Biaya Pasang (rp) 		= " <<BiayaPG<<endl;
+	TotalB = BiayaP+BiayaPG;
+	cout<< " Total Biaya (rp)		=" <<TotalB<<endl;
 }
