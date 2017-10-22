233ea397d152fafd44c574364bf3150ebbbc7dd5
shintiasari <32458115+shintiasari@users.noreply.github.com>
2017-10-21 22:32:37 -0700

    menampilkaninisialisisasi nilai yang sudah ditentukan

--- praktek 0204.cpp
+++ praktek 0204.cpp
@@ -5,4 +5,6 @@ int main(){
 	double LuasRumah = 42;
 	double LantaiPkt = 193*1215;
 	double Luas1Box = LantaiPkt*8/1000000;
+	cout << " Luas Rumah			= "<<LuasRumah<<" m2"<<endl;
+	cout << " Luas 1 Box Parket		= "<<Luas1Box<<" m2"<<endl;
 }
