3f2dac655bf3bcd585c9f09eae3697ace896f816
shintiasari <32458115+shintiasari@users.noreply.github.com>
2017-10-21 22:08:01 -0700

    Output bermacam gaya

--- praktek 0202.cpp
+++ praktek 0202.cpp
@@ -11,4 +11,8 @@ int niLAi=3;
 double angka(10.5),Angka,ANGKA(3.14);
 int a(10),b(7),c(12);
 
+cout << nilai<<" : "<<Nilai<<" "<<Nilai<<" "<<niLAi<<endl;
+cout << angka <<"\n";
+cout << Angka <<endl<<ANGKA;
+
 }
